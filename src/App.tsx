409ba1b16de598a21b3f9ba0bb230c123fe30344
Komail <komail.f67@gmail.com>
2022-07-14 01:28:33 +0430

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import SearchPage from './pages/search';
+import HistoryPage from './pages/history';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/search" element={<SearchPage />} />
+      <Route path="/history" element={<HistoryPage />} />
+    </Routes>
   );
 }
 
