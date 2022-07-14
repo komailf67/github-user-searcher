d60fbbf0ea5b6f0b194b6c6a6c991a7d3522bfde
Komail <komail.f67@gmail.com>
2022-07-15 02:57:09 +0430

    change fetcher structure

--- src/hooks/useSearchUser.ts
+++ src/hooks/useSearchUser.ts
@@ -1,34 +1,75 @@
 import { useState } from 'react';
-import { ISucceedSearchUserResDTO } from 'types/DTO/searchUser';
+import {
+  IFaildSearchUserResDTO,
+  IRepoItem,
+  ISucceedSearchUserResDTO,
+  TUserDetails,
+} from 'types/DTO/searchUser';
+
+const baseUrl = process.env.REACT_APP_BASE_URL;
 
 export const useSearchUser = () => {
   const [isFetching, setIsFetching] = useState<boolean>(false);
-  const [userDetails, setUserDetails] =
-    useState<ISucceedSearchUserResDTO | null>(null);
+  const [userDetails, setUserDetails] = useState<TUserDetails | null>(null);
   const [error, setError] = useState<string | null>(null);
   const fetcher = async (userName: string) => {
     setIsFetching(true);
-    return await fetch(`https://api.github.com/users/${userName}`).then(
-      (response) => {
+    await fetch(`${baseUrl}users/${userName}`)
+      .then((response) => {
         response
           .json()
-          .then((data) => {
+          .then((data: ISucceedSearchUserResDTO & IFaildSearchUserResDTO) => {
             if (!response.ok) {
-              setUserDetails(null);
-              setError(data.message);
+              throw new Error(data.message);
             } else {
               setError(null);
-              setUserDetails(data);
+              fetchUserRepos(userName, data);
             }
             setIsFetching(false);
           })
           .catch((err) => {
+            setError(`${err.message}: error in fetching user info`);
             setIsFetching(false);
             setUserDetails(null);
-            setError('something went wrong');
           });
-      }
-    );
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsFetching(false);
+        setUserDetails(null);
+      });
   };
+  const fetchUserRepos = async (
+    userName: string,
+    userInfo: ISucceedSearchUserResDTO
+  ) => {
+    setIsFetching(true);
+    return await fetch(`${baseUrl}users/${userName}/repos`)
+      .then((response) => {
+        response
+          .json()
+          .then((data: IRepoItem[] & IFaildSearchUserResDTO) => {
+            setIsFetching(false);
+            if (!response.ok) {
+              throw new Error(data.message);
+            } else {
+              setUserDetails({ ...userInfo, repos: data });
+              setError(null);
+              return data;
+            }
+          })
+          .catch((err) => {
+            setError(`${err.message} error in fetching repositories`);
+            setIsFetching(false);
+            setUserDetails(null);
+          });
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsFetching(false);
+        setUserDetails(null);
+      });
+  };
+
   return { isFetching, fetcher, userDetails, error };
 };
